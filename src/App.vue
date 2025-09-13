6f1982d744430e4aa7d47cc6b8d1bf04065322aa
M3galodn81 <dalisdism0@gmail.com>
2025-09-13 21:59:18 +0800

    add shadcn fr

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,53 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+</script>
+
 <template>
-    <div>
-        
+    <div class="min-h-screen bg-amber-200 ">
+        <section class="pt-32 pb-16 px-16 ">
+            <!-- Hero -->
+            <div class="relative ">
+                <div class="absolute inset-0 flex items-center justify-center ">
+                    <div class=" text-[10rem] bg-gradient-to-t from-blue-50 via-blue-500 to-red-500 bg-clip-text">
+                     Emoji 
+                     </div>
+                    
+                </div>
+            </div>
+
+            <!-- Content -->
+            <div class="relative z-10 text-center py-20">
+                <h1 class="text-black">
+                    Test
+                </h1>
+            </div>
+        </section>
+
+        <Card>
+            <CardHeader>
+                <CardTitle> Wawa</CardTitle>
+                <CardDescription>Omg gdo</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+                Card Content
+            </CardContent>
+
+            <CardFooter>
+                romanji
+            </CardFooter>
+
+        </Card>
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-    setup () {
-        
-
-        return {}
-    }
-})
-</script>
 
 <style scoped>
 
